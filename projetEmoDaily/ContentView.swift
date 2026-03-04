c68ab1306f8cf3c0b7692dccfbc448c4f1583d64
Julien H. <hojulien02@gmail.com>
2026-03-04 14:57:00 +0100

    Base du projet

---
+++ projetEmoDaily/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  projetEmoDaily
+//
+//  Created by Apprenent 151 on 04/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ projetEmoDaily/projetEmoDailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  projetEmoDailyApp.swift
+//  projetEmoDaily
+//
+//  Created by Apprenent 151 on 04/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct projetEmoDailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
