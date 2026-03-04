23110ae3b58b3d5c9efb04646d8f78c3288ad4e3
Krystal <krys.b@outlook.com>
2026-03-04 15:10:56 +0100

    Création de la vue pour l'écran "Nouvelle entréee"

---
+++ projetEmoDaily/EcranNouvelleEntree.swift
@@ -0,0 +1,18 @@
+//
+//  EcranNouvelleEntree.swift
+//  projetEmoDaily
+//
+//  Created by Apprenant 148 on 04/03/2026.
+//
+
+import SwiftUI
+
+struct EcranNouvelleEntree: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EcranNouvelleEntree()
+}
