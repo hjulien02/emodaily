82464b9d150a708fa5960bf70f2ea070bacf81b2
Julien H. <hojulien02@gmail.com>
2026-03-11 16:46:30 +0100

    Ajout de la connection à l'API via les ViewModels

---
+++ projetEmoDaily/ExtractedViews/Components/Title.swift
@@ -0,0 +1,18 @@
+//
+//  Title.swift
+//  projetEmoDaily
+//
+//  Created by Apprenent 151 on 10/03/2026.
+//
+
+import SwiftUI
+
+struct Title: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Title()
+}
